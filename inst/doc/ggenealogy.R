8b28bf1d5e7b22bbbbb75af0e408ca7b0bcd3ba0
lrutter <lindsayannerutter@gmail.com>
2015-02-23 17:21:28 -0600

    Changing the vignette to Sweave .rnw file. Not using knitrBootstrap for time being.

--- inst/doc/ggenealogy.R
+++ inst/doc/ggenealogy.R
@@ -1,23 +1,62 @@
-## ------------------------------------------------------------------------
-options(warn=-1)
-library(ggenealogy)
-library(ggplot2)
-library(stringr)
-library(knitr)
+### R code from vignette source 'ggenealogy.Rnw'
 
-## ------------------------------------------------------------------------
+###################################################
+### code chunk number 1: ggenealogy.Rnw:55-56
+###################################################
+rm(list=ls())
+
+
+###################################################
+### code chunk number 2: ggenealogy.Rnw:85-86
+###################################################
+library(ggenealogy)
+
+
+###################################################
+### code chunk number 3: ggenealogy.Rnw:91-93 (eval = FALSE)
+###################################################
+## help(package="ggenealogy")
+## help(ggenealogy)
+
+
+###################################################
+### code chunk number 4: ggenealogy.Rnw:102-103 (eval = FALSE)
+###################################################
+## example(ggenealogy)
+
+
+###################################################
+### code chunk number 5: ggenealogy.Rnw:110-113
+###################################################
 data(sbTree)
 head(sbTree)
+class(sbTree)
 
-## ------------------------------------------------------------------------
-ig <- processTreeGraph(sbTree)
 
-## ------------------------------------------------------------------------
-ig <- processTreeGraph(sbTree, vertexinfo=c("year", "yield", "year.imputed", "min.repro.year"))
+###################################################
+### code chunk number 6: ggenealogy.Rnw:120-121
+###################################################
+help(treeToIG)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 7: ggenealogy.Rnw:128-130
+###################################################
+ig <- treeToIG(sbTree)
+class(ig)
+
+
+###################################################
+### code chunk number 8: ggenealogy.Rnw:137-138
+###################################################
+ig <- treeToIG(sbTree, vertexinfo=c("year", "yield", "year.imputed"))
+
+
+###################################################
+### code chunk number 9: ggenealogy.Rnw:143-155
+###################################################
 library(plyr)
-nodes <- unique(sbTree[,1:5])
+nodes <- unique(sbTree[,1:4])
 
 # get a data frame of all parents whose parents are not known (i.e. parents who are not listed as children as well)
 extra.nodes <- unique(data.frame(child=sbTree$parent[!sbTree$parent%in%sbTree$child & !is.na(sbTree$parent)], stringsAsFactors=FALSE))
@@ -27,93 +66,252 @@ nodes <- rbind.fill(nodes, extra.nodes)
 rm(extra.nodes)
 
 # We can now specify our vertex information using the data frame nodes: 
-ig <- processTreeGraph(sbTree, vertexinfo=nodes)
+ig <- treeToIG(sbTree, vertexinfo=nodes)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 10: ggenealogy.Rnw:170-172
+###################################################
 isParent("Young","Essex",sbTree)
 isParent("Essex","Young",sbTree)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 11: ggenealogy.Rnw:179-181
+###################################################
 isChild("Young","Essex",sbTree)
 isChild("Essex","Young",sbTree)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 12: ggenealogy.Rnw:188-190
+###################################################
 getYear("Young",sbTree)
 getYear("Essex",sbTree)
 
-## ------------------------------------------------------------------------
-getparent("Young",sbTree)
-getparent("Tokyo",sbTree)
+
+###################################################
+### code chunk number 13: ggenealogy.Rnw:197-200
+###################################################
+getParent("Young",sbTree)
+getParent("Tokyo",sbTree)
 getYear("Tokyo", sbTree)
 
-## ------------------------------------------------------------------------
-getchild("Tokyo",sbTree)
-getchild("Ogden",sbTree)
 
-## ------------------------------------------------------------------------
+###################################################
+### code chunk number 14: ggenealogy.Rnw:207-209
+###################################################
+getChild("Tokyo",sbTree)
+getChild("Ogden",sbTree)
+
+
+###################################################
+### code chunk number 15: ggenealogy.Rnw:218-219
+###################################################
+getAncestors("Young",sbTree,1)
+
+
+###################################################
+### code chunk number 16: ggenealogy.Rnw:226-228
+###################################################
+getAncestors("Young",sbTree,5)
+dim(getAncestors("Young",sbTree,5))
+
+
+###################################################
+### code chunk number 17: ggenealogy.Rnw:235-236
+###################################################
+getDescendants("Ogden",sbTree,1)
+
+
+###################################################
+### code chunk number 18: ggenealogy.Rnw:243-244
+###################################################
+getDescendants("Ogden",sbTree,2)
+
+
+###################################################
+### code chunk number 19: ggenealogy.Rnw:253-255
+###################################################
 getDegree("Tokyo", "Ogden", ig, sbTree)
 getDegree("Tokyo", "Holladay", ig, sbTree)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 20: ggenealogy.Rnw:266-267
+###################################################
 getBasicStatistics(ig)
 
-## ------------------------------------------------------------------------
+
+###################################################
+### code chunk number 21: ggenealogy.Rnw:274-277
+###################################################
+eList = getEdges(ig, sbTree)
+head(eList)
+dim(eList)
+
+
+###################################################
+### code chunk number 22: ggenealogy.Rnw:282-285
+###################################################
+nList = getNodes(sbTree)
+head(nList)
+length(nList)
+
+
+###################################################
+### code chunk number 23: plotAncDes1
+###################################################
+plotAncDes("Lee", sbTree, 3, 5)
+
+
+###################################################
+### code chunk number 24: plotAncDes1
+###################################################
+plotAncDes("Lee", sbTree, 3, 5)
+
+
+###################################################
+### code chunk number 25: plotAncDes2
+###################################################
+plotAncDes("Tokyo", sbTree, vColor = "blue") + ggplot2::labs(x="Generation index",y="")
+
+
+###################################################
+### code chunk number 26: plotAncDes2
+###################################################
+plotAncDes("Tokyo", sbTree, vColor = "blue") + ggplot2::labs(x="Generation index",y="")
+
+
+###################################################
+### code chunk number 27: ggenealogy.Rnw:348-349
+###################################################
 getPath("Brim","Bedford", ig, sbTree, isDirected=FALSE)
 
-## ------------------------------------------------------------------------
-dirgraph <- processTreeGraph(sbTree, vertexinfo=nodes, isDirected = TRUE)
-getPath("Brim", "Bedford", dirgraph, isDirected=TRUE)
 
-## ------------------------------------------------------------------------
-getPath("Bedford", "Brim", dirgraph, isDirected=TRUE)
+###################################################
+### code chunk number 28: ggenealogy.Rnw:356-358
+###################################################
+dirIG = treeToIG(sbTree, vertexinfo=nodes, isDirected = TRUE)
+getPath("Brim", "Bedford", dirIG, sbTree, isDirected = TRUE)
 
-## ------------------------------------------------------------------------
-path <- getPath("Brim","Bedford", ig, sbTree, isDirected=F)
 
-## ------------------------------------------------------------------------
-plotPathDF <- plotPath(path)
+###################################################
+### code chunk number 29: ggenealogy.Rnw:363-364
+###################################################
+getPath("Bedford", "Brim", dirIG, sbTree, isDirected=TRUE)
 
-## ------------------------------------------------------------------------
-dirgraph <- processTreeGraph(sbTree, vertexinfo=nodes, isDirected = TRUE)
 
-path <- getPath("Narow", "Tokyo", dirgraph, sbTree, isDirected=TRUE)
-plotPathImage <- plotPath(path)
-plotPathImage
+###################################################
+### code chunk number 30: ggenealogy.Rnw:371-372
+###################################################
+path = getPath("Bedford","Brim", ig, sbTree, isDirected=FALSE)
 
-## ------------------------------------------------------------------------
-binVector <- c(1,4,7,10,2,5,8,11,3,6,9,12)
-spreadTotalDF <- buildSpreadTotalDF(ig, binVector)
-head(spreadTotalDF)
 
-## ------------------------------------------------------------------------
-plotMinusPathDF <- buildMinusPathDF(path, ig)
-head(plotMinusPathDF)
+###################################################
+### code chunk number 31: plotPath1
+###################################################
+plotPath(path)
 
-## ------------------------------------------------------------------------
-edgeTotalDF <- buildEdgeTotalDF(ig)
-head(edgeTotalDF)
 
-## ------------------------------------------------------------------------
-plotTotalDF <- buildPlotTotalDF(path, ig)
-head(plotTotalDF)
+###################################################
+### code chunk number 32: plotPath1
+###################################################
+plotPath(path)
 
-## ------------------------------------------------------------------------
-plotTotalImage <- plotPathOnTree(path=path, ig=ig)
-plotTotalImage
 
-## ------------------------------------------------------------------------
-plotTotalImage <- plotPathOnTree(path=path, ig=ig, binVector=1:12)
+###################################################
+### code chunk number 33: plotPath2
+###################################################
+path = getPath("Narow", "Tokyo", ig, sbTree, isDirected=FALSE)
+plotPath(path)
 
-## ------------------------------------------------------------------------
-plotTotalImage <- plotPathOnTree(path=path, ig=ig, binVector=1:2)
 
-## ------------------------------------------------------------------------
-varieties <- c("Beeson", "Brim", "Dillon", "Narow", "Zane", "Hood", "York", "Calland", "Columbus", "Crawford", "Kershaw", "Kent","Bragg", "Davis","Tokyo","Hagood","Young","Essex","Holladay","Cook","Century","Pella","Forrest","Gasoy","Cutler")
-heatMapDegreeImage <- plotDegMatrix(varieties,ig,sbTree)
-heatMapDegreeImage
+###################################################
+### code chunk number 34: plotPath2
+###################################################
+path = getPath("Narow", "Tokyo", ig, sbTree, isDirected=FALSE)
+plotPath(path)
 
-## ------------------------------------------------------------------------
-varieties <- c("Beeson", "Brim", "Dillon", "Narow", "Zane", "Hood", "York", "Calland", "Columbus", "Crawford", "Kershaw", "Kent","Bragg", "Davis","Tokyo","Hagood","Young","Essex","Holladay","Cook","Century","Pella","Forrest","Gasoy","Cutler")
-heatMapYearImage <- plotYearMatrix(varieties,sbTree)
-heatMapYearImage
+
+###################################################
+### code chunk number 35: ggenealogy.Rnw:416-418
+###################################################
+path = getPath("Narow", "Tokyo", dirIG, sbTree, isDirected=TRUE)
+plotPath(path)
+
+
+###################################################
+### code chunk number 36: plotPath3
+###################################################
+path = getPath("Tokyo", "Narow", dirIG, sbTree, isDirected=TRUE)
+plotPath(path)
+
+
+###################################################
+### code chunk number 37: plotPath3
+###################################################
+path = getPath("Tokyo", "Narow", dirIG, sbTree, isDirected=TRUE)
+plotPath(path)
+
+
+###################################################
+### code chunk number 38: plotPathOnTree1
+###################################################
+plotPathOnTree(path, sbTree, ig, binVector = 1:3)
+
+
+###################################################
+### code chunk number 39: plotPathOnTree1
+###################################################
+plotPathOnTree(path, sbTree, ig, binVector = 1:3)
+
+
+###################################################
+### code chunk number 40: plotPathOnTree2
+###################################################
+plotPathOnTree(path, sbTree, ig, binVector = 1:6)
+
+
+###################################################
+### code chunk number 41: plotPathOnTree2
+###################################################
+plotPathOnTree(path, sbTree, ig, binVector = 1:6)
+
+
+###################################################
+### code chunk number 42: plotDegMatrix1
+###################################################
+varieties=c("Brim", "Bedford", "Calland", "Narow", "Pella", "Tokyo", "Young", "Zane")
+p = plotDegMatrix(varieties, ig, sbTree, "Soybean label", "Soybean label", "Degree")
+p + ggplot2::scale_fill_continuous(low="white", high="darkgreen")
+
+
+###################################################
+### code chunk number 43: plotDegMatrix1
+###################################################
+varieties=c("Brim", "Bedford", "Calland", "Narow", "Pella", "Tokyo", "Young", "Zane")
+p = plotDegMatrix(varieties, ig, sbTree, "Soybean label", "Soybean label", "Degree")
+p + ggplot2::scale_fill_continuous(low="white", high="darkgreen")
+
+
+###################################################
+### code chunk number 44: ggenealogy.Rnw:514-515
+###################################################
+getDegree("Bedford", "Zane", ig, sbTree)
+
+
+###################################################
+### code chunk number 45: plotYearMatrix1
+###################################################
+varieties=c("Brim", "Bedford", "Calland", "Narow", "Pella", "Tokyo", "Young", "Zane")
+plotYearMatrix(varieties,sbTree)
+
+
+###################################################
+### code chunk number 46: plotYearMatrix1
+###################################################
+varieties=c("Brim", "Bedford", "Calland", "Narow", "Pella", "Tokyo", "Young", "Zane")
+plotYearMatrix(varieties,sbTree)
+
 
